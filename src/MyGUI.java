e3c8f1ccbab362f50a9e426f78a950ecd574d55a
msicaja <msicaja1@students.avc.edu>
2025-02-22 17:54:23 -0800

    Updated to include welcome screen, buttons are currently not functional, will be added soon.

--- src/MyGUI.java
+++ src/MyGUI.java
@@ -1,15 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class MyGUI {
+
+    private static JLabel titleTxt;
+    private static JButton registerButton;
+    private static JButton loginButton;
     public MyGUI() {
+        /////////////// Initial GUI Settings /////////////
         JFrame frame = new JFrame();
-        frame.setSize(600, 600);
+        frame.setSize(1000, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // Centers the window
 
-        JPanel panel = new JPanel();
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints(); // Allows for ordered positioning
         frame.add(panel);
+        ///////////// Text ///////////
 
-        panel.setLayout(null);
+        // Title text
+        titleTxt = new JLabel("Welcome to Goober's Storage Incorporated!", SwingConstants.CENTER);
+        titleTxt.setFont(new Font("SansSerif", Font.BOLD, 40));
+        titleTxt.setMinimumSize(new Dimension(400, 400));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(titleTxt, gbc);
+
+        ////////////// Buttons ////////////
+
+        // Register Button
+        registerButton = new JButton("Register");
+        registerButton.setPreferredSize(new Dimension(180, 50));
+        gbc.gridy = 1; // Move button to next row
+        gbc.insets = new Insets(20, 0, 10, 0); // Add spacing in between buttons and labels
+        panel.add(registerButton, gbc);
+
+        // Login Button
+        loginButton = new JButton("Log-in");
+        loginButton.setPreferredSize(new Dimension(180, 50));
+        gbc.gridy = 2; // Move button to next row
+        gbc.insets = new Insets(20, 0, 10, 0); // Add spacing in between buttons and labels
+        panel.add(loginButton, gbc);
 
         frame.setVisible(true);
     }
