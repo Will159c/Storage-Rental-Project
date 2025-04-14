8e420ade61d6272db1eb676464919cf7ea79e3f9
Will159c <163231055+Will159c@users.noreply.github.com>
2025-04-14 16:34:42 -0700

    Made some small GUI changes, changed enter password - reenter password when reserving. Aswell as modified enter 16 digit number to enter 16 digit creditcard number.

--- src/MySQL.java
+++ src/MySQL.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class MySQL {
     private static final String URL = "jdbc:mysql://caboose.proxy.rlwy.net:54157/railway";
     private static final String USER = "root";
-    private static final String PASSWORD = "OaWunWeWjnACHWrhVxwAIQJVZPtotFuD"; // Replace with actual password
+    private static final String PASSWORD = "Replace Here"; // Replace with actual password
 
     private static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
