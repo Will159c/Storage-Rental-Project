d5d82a46d47091cc4f23b48b081a479c0ef1d0b5
alexisanguiano <alexisanguiano@ymail.com>
2025-04-15 18:02:57 -0700

    Javadoc info for DataLabelFormatter class.

--- src/DateLabelFormatter.java
+++ src/DateLabelFormatter.java
@@ -3,14 +3,36 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+/**
+ * A) Class name: DateLabelFormatter
+ * B) Date of the Code: March 30 ,2025
+ * C) Programmer's name: Alexis Anguiano
+ * D) Brief description: Utility class used by JDatePicker to convert between Date and String. It handles formatting user selected dates and parsing back into usable values.
+ * E) Brief explanation of important function: stringToValue parses a string to a date object. valueToString formats a date to a string.
+ * F) Important ata structures: Uses simpledateformat internally with fixed format: m-d-y
+ * G) Algorithm used: simpledateformat
+ */
+
 public class DateLabelFormatter extends JFormattedTextField.AbstractFormatter {
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
 
+    /**
+     * Converts a string to a {@link java.util.Date}.
+     * @param text
+     * @return
+     * @throws ParseException
+     */
     @Override
     public Object stringToValue(String text) throws ParseException {
         return dateFormat.parse(text);
     }
 
+    /**
+     * Converts a {@link java.util.Date} to a string
+     * @param value
+     * @return
+     * @throws ParseException
+     */
     @Override
     public String valueToString(Object value) throws ParseException {
         if (value != null) {
